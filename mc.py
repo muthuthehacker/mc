105a200e9fdc8c00d84b1b832993fa2cc03ac1a6
muthu_the_hacker <a.muthuchidambaram2001@gmail.com>
2022-01-22 20:07:28 +0530

    first commit

---
+++ mc.py
@@ -0,0 +1,4 @@
+print("Whatsapp Database Fetching in ......  6374815326")
+print("Export Media ......  ")
+print("Data = 6.7GB... Minimum Bandwidth speed = 50 MBBS")
+print("This is true allow for continue otherwise please exit do dry file is corupted ......")
